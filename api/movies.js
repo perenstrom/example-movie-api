04ca6cfcf7b5717293cb0f16e24680b6d4e30d4f
Per Enström <enstrom.per@gmail.com>
2021-04-05 14:22:15 +0200

    Add more cors headers

--- api/movies.js
+++ api/movies.js
@@ -3,7 +3,16 @@ const delayResponse = require("../helpers/delay").delay;
 
 module.exports = (req, res) => {
   if (req.method === "GET") {
+    res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
     const {
       query: { delay = "5000" },
     } = req;
