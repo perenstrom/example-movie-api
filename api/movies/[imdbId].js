f51b19557e1d833ea6e6d8632d2940b047c3af03
Per Enström <enstrom.per@gmail.com>
2021-04-05 14:39:43 +0200

    Add options response

--- api/movies/[imdbId].js
+++ api/movies/[imdbId].js
@@ -2,17 +2,20 @@ const fixtures = require("../../fixtures/movies");
 const delayResponse = require("../../helpers/delay").delay;
 
 module.exports = (req, res) => {
-  if (req.method === "GET") {
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  } else if (req.method === "GET") {
     const {
       query: { imdbId, delay = "5000" },
     } = req;
